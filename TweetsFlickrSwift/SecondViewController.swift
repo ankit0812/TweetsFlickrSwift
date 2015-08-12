2e16a7fe732177c8e67350e6edb56186b491a085
ankitoptimus <ankitkr.gupta@optimusinfo.com>
2015-08-12 18:49:28 +0530

    " Fetched images of sender , name and Tweet .. working on layout "

--- TweetsFlickrSwift/SecondViewController.swift
+++ TweetsFlickrSwift/SecondViewController.swift
@@ -15,19 +15,18 @@ import Social
 
 class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-//    @IBOutlet var searchTextField: UITextField!
     @IBOutlet var tweetTableView: UITableView!
     
     var dataSource = [AnyObject]()
-    
-    let basicCellIdentifier = "TwitterCell"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.tweetTableView.registerClass(UITableViewCell.self,
-            forCellReuseIdentifier:basicCellIdentifier)
+        self.tweetTableView.estimatedRowHeight=200
+        
+        self.tweetTableView.rowHeight = UITableViewAutomaticDimension
         
         self.getTimeLine()
     }
@@ -38,59 +37,7 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     
     }
-//    
-//    func getTimeLine() {
-//        
-//        let account = ACAccountStore()
-//        let accountType = account.accountTypeWithAccountTypeIdentifier(
-//            ACAccountTypeIdentifierTwitter)
-//        
-//        account.requestAccessToAccountsWithType(accountType, options: nil,
-//            completion: {(success: Bool, error: NSError!) -> Void in
-//                
-//                if success {
-//                    let arrayOfAccounts =
-//                    account.accountsWithAccountType(accountType)
-//                    
-//                    if arrayOfAccounts.count > 0 {
-//                        let twitterAccount = arrayOfAccounts.last as! ACAccount
-//                        
-//                        let requestURL = NSURL(string:
-//                            "https://api.twitter.com/1.1/statuses/user_timeline.json")
-//                        
-//                        let parameters = ["screen_name" : "@techotopia",
-//                            "include_rts" : "0",
-//                            "trim_user" : "1",
-//                            "count" : "20"]
-//                        
-//                        let postRequest = SLRequest(forServiceType:
-//                            SLServiceTypeTwitter,
-//                            requestMethod: SLRequestMethod.GET,
-//                            URL: requestURL,
-//                            parameters: parameters)
-//                        
-//                        postRequest.account = twitterAccount
-//                        
-//                        postRequest.performRequestWithHandler(
-//                            {(responseData: NSData!,
-//                                urlResponse: NSHTTPURLResponse!,
-//                                error: NSError!) -> Void in
-//                                var err: NSError?
-//                                self.dataSource = NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.MutableLeaves, error: &err) as! [AnyObject]
-//                                
-//                                if self.dataSource.count != 0 {
-//                                    dispatch_async(dispatch_get_main_queue()) {
-//                                        self.tableView.reloadData()
-//                                    }
-//                            }
-//                        })
-//                    }
-//                } else {
-//                    println("Failed to access account")
-//                }
-//        })
-//    }
-    
+   
     func getTimeLine() {
         
         let account = ACAccountStore()  // getting the account store
@@ -134,22 +81,27 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         })
     }
     
-     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return dataSource.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell =
-        self.tweetTableView.dequeueReusableCellWithIdentifier(basicCellIdentifier)
-            as! UITableViewCell
+        let cell = self.tweetTableView.dequeueReusableCellWithIdentifier("TwitterCell")
+            as! TwitterCell
         
         let row = indexPath.row
+        
         let tweet = self.dataSource[row] as! NSDictionary
+        let userInfo = tweet.objectForKey("user") as! NSDictionary
         
-        cell.textLabel!.text = tweet.objectForKey("text") as? String
-        cell.textLabel!.numberOfLines = 0
-        
+        cell.textTweet!.text = tweet.objectForKey("text") as? String
+        cell.nameTweet!.text = userInfo.objectForKey("name") as? String
+        var imageInfo : NSString = (userInfo.objectForKey("profile_image_url") as? String)!
+        let url = NSURL(string: imageInfo as String)
+        let data = NSData(contentsOfURL: url!)
+        cell.imageSender.image = UIImage(data: data!)
+       
         return cell
     }
 }
